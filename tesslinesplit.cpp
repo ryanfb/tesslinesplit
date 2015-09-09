1416f2477221546678a6e898366316aa98c920fb
dhg <dhgarrette@gmail.com>
2015-09-08 22:50:19 -0700

    edited for changes to ocular

--- tesslinesplit.cpp
+++ tesslinesplit.cpp
@@ -4,22 +4,36 @@
 #include <libgen.h>
 
 int main(int argc, char *argv[]) {
+  if(argc != 3) { printf("USAGE: %s imagefile outputdir", argv[0]); }
+
   std::string input_filename = std::string(argv[1]);
   std::string input_basename = std::string(basename(argv[1]));
+
+  std::string output_dir = std::string(argv[2]);
+
   Pix *image = pixRead(input_filename.c_str());
   tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
   api->Init(NULL, "eng");
   api->SetImage(image);
   Boxa* boxes = api->GetComponentImages(tesseract::RIL_TEXTLINE, true, NULL, NULL);
   printf("Found %d textline image components.\n", boxes->n);
+
+  int lastindex = input_basename.find_last_of(".");
+  std::string basename = input_basename.substr(0, lastindex).c_str();
+  std::string extension = input_basename.substr(lastindex + 1).c_str();
+  std::string outdir = output_dir.c_str();
+  int linedirLen = outdir.length()+1+basename.length()+strlen("-line_extract_")+extension.length()+1;
+  char* linedir = (char*)malloc(linedirLen*sizeof(char));
+  sprintf(linedir, "%s/%s-line_extract_%s", output_dir.c_str(), basename.c_str(), extension.c_str());
+  printf("Writing to directory [%s].  If this fails, try:\n    mkdir -p %s\n\n\n", linedir, linedir);
+
   for (int i = 0; i < boxes->n; i++) {
     BOX* box = boxaGetBox(boxes, i, L_CLONE);
     PIX* pixd= pixClipRectangle(image, box, NULL);
-    int lastindex = input_basename.find_last_of(".");
-    std::string basename = input_basename.substr(0, lastindex);
-    std::string extension = input_basename.substr(lastindex + 1, 3);
-    char* linefilename = (char*)malloc((basename.length()+strlen("-line_extract-00000.ext")+1)*sizeof(char));
-    sprintf(linefilename, "%s-line_extract-%05d.%s", basename.c_str(), i, extension.c_str());
+
+    char* linefilename = (char*)malloc((linedirLen+strlen("/line00.")+extension.length()+1)*sizeof(char));
+    sprintf(linefilename, "%s/line%02d.%s", linedir, i, extension.c_str());
+    printf("  Writing %s\n", linefilename);
     pixWrite(linefilename, pixd, IFF_DEFAULT);
     pixDestroy(&pixd);
     boxDestroy(&box);
